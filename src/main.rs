297ff6f925d94ae87e522684b694cc789f58133b
Ian McBee <imcbee@terpmail.umd.edu>
2022-12-25 13:04:49 -0700

    completed chapter 1 and learning hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 fn main() {
-    println!("Hello, world!");
+    let my_list = ["One", "Two", "Three"];
+    for num in my_list {
+        println!("{}", num)
+    }
+    //println!("Hello, world!"); // exclaimation point means macro}
 }
